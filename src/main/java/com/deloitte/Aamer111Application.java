1a599576ad326ad5302e3771fe717d9f003e915c
shaikhaamer <89386754+shaikhaamer@users.noreply.github.com>
2021-10-12 19:10:41 +0530

    Initial Commit

---
+++ src/main/java/com/deloitte/Aamer111Application.java
@@ -0,0 +1,13 @@
+package com.deloitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aamer111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aamer111Application.class, args);
+	}
+
+}
